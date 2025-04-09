86fe03520bd6280c9cfe7da6c1c15cf212b1b5ca
AnujSinghR <anujsingh0r@gmail.com>
2025-04-09 10:27:23 +0530

    new changes

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyDNbSCZSdayV7WggnWhPgTo_rgpOQARllQ",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "url-shortner-8465f.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "url-shortner-8465f",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "url-shortner-8465f.firebasestorage.app",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "792443660508",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:792443660508:web:2a612a546735891492ac19"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
